365dc3bccb8a954522620b4b6e1f91c895723a42
joe <brakejoe9@gmail.com>
2024-03-31 07:01:44 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+
+function saturdayFun(target = "roller-skate") {
+    return `This Saturday, I want to ${target}!`;
+  }
+  
+  console.log(saturdayFun()); 
+  console.log(saturdayFun("bathe my dog"));
+
+
+
+  function mondayWork(target = "go to the office") {
+    return`This Monday, I will ${target}.`;
+  }
+  console.log(mondayWork()); 
+  console.log(mondayWork("work from home"));
+
+
+
+
+
+  function wrapAdjective(flair = "*") {
+    return function(msg = "special") {
+        return `You are ${flair}${msg}${flair}!`;
+    };
+}
+
+
+console.log(wrapAdjective("%")("a dedicated programmer"));
+  
+
+
+  
